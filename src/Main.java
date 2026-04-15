90257e19e7b0e676e6958f3532e212d8fa17dca0
onjamm <75151498+onjamm@users.noreply.github.com>
2026-04-15 14:03:11 -0700

    Completed timesTable

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ import java.util.Set;
 public class Main {
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(n^2) or should I say O(x^2)
+  //with x representing the size of the times table
   public static void timesTable(int x) {
     for(int i = 1; i <= x; i++) {
         for(int j = 1; j <= x; j++) {
